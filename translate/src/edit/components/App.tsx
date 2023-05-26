0a5e428b1160312a28588e7497124299620920d0
TED.F <107335268+0824F@users.noreply.github.com>
2023-05-26 20:45:29 +0800

    Init project and add docker file
    init project and add docker file

---
+++ translate/src/edit/components/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "../edit.css";
+
+const App = () => {
+  return <div>edit</div>;
+};
+
+export default App;
---
+++ translate/src/taskpane/components/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App = () => {
+  return <div className="w-full bg-blue-300 text-center">home</div>;
+};
+
+export default App;
