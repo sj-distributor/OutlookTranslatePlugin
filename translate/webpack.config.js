60e4d346027e5d611cd337ab2f9407f62f23377c
TED.F <107335268+0824F@users.noreply.github.com>
2023-05-30 08:30:35 +0800

    update webpack

--- translate/webpack.config.js
+++ translate/webpack.config.js
@@ -85,6 +85,10 @@ module.exports = async (env, options) => {
               }
             },
           },
+          {
+            from: "appsetting.json",
+            to: "appsetting.json",
+          },
         ],
       }),
       new HtmlWebpackPlugin({
