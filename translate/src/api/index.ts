65d0cf58f784485224aa3f303a9a7cab08e0747d
TED.F <107335268+0824F@users.noreply.github.com>
2023-05-30 18:08:39 +0800

    update code

--- translate/src/api/index.ts
+++ translate/src/api/index.ts
@@ -1,21 +1,41 @@
 import { AppSettings } from "../../appsettings";
+import axios from "axios";
 
 // /api/Google/translate data: any  JSON.stringify(data)
 export const PostTranslate = (content: string) => {
   const settings = (window as any).appsettings as AppSettings;
-  return fetch(`${settings.serverUrl}/api/Google/translate`, {
-    method: "post",
-    body: JSON.stringify({
-      content: content,
-      targetLanguage: "zh-TW",
-    }),
-    headers: {
-      "X-API-KEY": settings.apiKey,
-    },
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      return res;
+  return axios
+    .post(
+      `${settings.serverUrl}/api/Google/translate`,
+      {
+        content: content,
+        targetLanguage: "zh-TW",
+      },
+      {
+        headers: {
+          "X-API-KEY": settings.apiKey,
+        },
+      }
+    )
+    .then(function (response) {
+      console.log(response);
     })
-    .catch((err) => console.log(err, "err"));
+    .catch(function (error) {
+      console.log(error);
+    });
+  // return fetch(`${settings.serverUrl}/api/Google/translate`, {
+  //   method: "post",
+  //   body: {
+  //     content: content,
+  //     targetLanguage: "zh-TW",
+  //   },
+  //   headers: {
+  //     "X-API-KEY": settings.apiKey,
+  //   },
+  // })
+  //   .then((res) => res.json())
+  //   .then((res) => {
+  //     return res;
+  //   })
+  //   .catch((err) => console.log(err, "err"));
 };
