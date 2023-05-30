bd864b3f551684e48e87ec8762a9768026f40bb2
TED.F <107335268+0824F@users.noreply.github.com>
2023-05-30 14:23:33 +0800

    update code

--- translate/src/taskpane/components/hook.ts
+++ translate/src/taskpane/components/hook.ts
@@ -29,6 +29,7 @@ export const useAction = () => {
 
   const translateContent = () => {
     const text = JSON.stringify(cleanContent);
+    console.log(text, "text-translate");
     PostTranslate(text).then((res) => console.log(res, "res"));
   };
 
