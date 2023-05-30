f3b9dce76c7eae9514538356e23bcecc53aa4c14
TED.F <107335268+0824F@users.noreply.github.com>
2023-05-30 08:54:45 +0800

    update appsetting.ts

--- translate/appsettings.ts
+++ translate/appsettings.ts
@@ -8,9 +8,13 @@ const settings = (window as any).appsettings;
 export async function InitialAppSetting() {
   if ((window as any).appsettings) return (window as any).appsettings;
 
-  const appSettingsData = require("./appsetting.json");
+  // const appSettingsData = require("./appsetting.json");
 
-  (window as any).appsettings = appSettingsData;
+  fetch("./appsetting.json")
+    .then((response) => response.json())
+    .then((data: AppSettings) => {
+      (window as any).appsettings = data;
+    });
 }
 
 export default settings as AppSettings;
