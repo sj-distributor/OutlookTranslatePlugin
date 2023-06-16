10e0a5f94bebd38dcba4dfd3a085dc226d5e2ddb
TED.F <107335268+0824F@users.noreply.github.com>
2023-06-16 14:49:22 +0800

    update check code

--- translate/src/taskpane/components/App.tsx
+++ translate/src/taskpane/components/App.tsx
@@ -1,27 +1,12 @@
 import * as React from "react";
 import { useAction } from "./hook";
 import "../taskpane.css";
-import { Button, Select } from "antd";
 
 const App = () => {
-  const { content, translateContent, handleChange, language } = useAction();
+  const { content } = useAction();
 
   return (
-    <div className="w-full h-screen flex flex-col overflow-auto">
-      <div className="w-full flex flex-row">
-        <Select
-          className="w-full"
-          value={language}
-          onChange={handleChange}
-          options={[
-            { value: "zh-TW", label: "zh-TW" },
-            { value: "en", label: "en" },
-            { value: "es", label: "es" },
-          ]}
-        />
-        <Button onClick={translateContent}>translate</Button>
-      </div>
-
+    <div className="w-full h-screen flex flex-col overflow-auto p-1">
       <div dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
--- translate/src/taskpane/components/hook.ts
+++ translate/src/taskpane/components/hook.ts
@@ -1,21 +1,17 @@
 import { useEffect, useState } from "react";
-import { clone } from "ramda";
 import { PostTranslate } from "../../api";
+import { message } from "antd";
 
 export const useAction = () => {
   const [content, setContent] = useState<string>("");
 
-  const [cleanContent, setCleanContent] = useState<string>("");
-
-  const [language, setLanguage] = useState<string>("zh-Tw");
-
   useEffect(() => {
     Office.context.mailbox.item.body.getAsync("html", function callback(result) {
-      const clean = clone(result.value);
-      setCleanContent(clean);
-
-      replaceImg(clean);
-      // setContent(result.value);
+      if (result.status === Office.AsyncResultStatus.Succeeded) {
+        PostTranslate(result.value, "zh-TW").then((res) => {
+          replaceImg(res);
+        });
+      }
     });
   }, []);
 
@@ -29,54 +25,47 @@ export const useAction = () => {
 
     const promises = [];
 
-    for (let i = 0; i < imgTags.length; i++) {
-      const img = imgTags[i];
-      const src = img.getAttribute("src");
-
-      if (src) {
-        const attachmentId = attachments[i].id;
-        const promise = new Promise((resolve, reject) => {
-          Office.context.mailbox.item.getAttachmentContentAsync(attachmentId, (result) => {
-            if (result.status === Office.AsyncResultStatus.Succeeded) {
-              resolve(result.value.content);
-            } else {
-              reject(new Error("Failed to get attachment content."));
-            }
+    if (attachments.length <= 0 || imgTags.length <= 0) {
+      setContent(html);
+    } else {
+      for (let i = 0; i < imgTags.length; i++) {
+        const img = imgTags[i];
+        const src = img.getAttribute("src");
+        if (src) {
+          message.info(attachments.length);
+          const attachmentId = attachments[i]?.id;
+          const promise = new Promise((resolve, reject) => {
+            Office.context.mailbox.item.getAttachmentContentAsync(attachmentId, (result) => {
+              if (result.status === Office.AsyncResultStatus.Succeeded) {
+                resolve(result.value.content);
+              } else {
+                reject(new Error("Failed to get attachment content."));
+              }
+            });
           });
-        });
 
-        promises.push(promise);
+          promises.push(promise);
+        }
       }
+
+      Promise.all(promises)
+        .then((base64Contents) => {
+          for (let i = 0; i < imgTags.length; i++) {
+            const img = imgTags[i];
+            img.setAttribute("src", "data:image/png;base64," + base64Contents[i]);
+          }
+
+          const updatedHtml = tempDiv.innerHTML;
+          setContent(updatedHtml);
+          if (tempDiv.parentNode) {
+            tempDiv.parentNode.removeChild(tempDiv);
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     }
-
-    Promise.all(promises)
-      .then((base64Contents) => {
-        for (let i = 0; i < imgTags.length; i++) {
-          const img = imgTags[i];
-          img.setAttribute("src", "data:image/png;base64," + base64Contents[i]);
-        }
-
-        const updatedHtml = tempDiv.innerHTML;
-        setContent(updatedHtml);
-        if (tempDiv.parentNode) {
-          tempDiv.parentNode.removeChild(tempDiv);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
   };
 
-  // 把原始html分段截取出需要翻译的text，调接口翻译，替换原始文本
-  const translateContent = async () => {
-    await PostTranslate(cleanContent, language).then((res) => {
-      replaceImg(res);
-    });
-  };
-
-  const handleChange = (value: string) => {
-    setLanguage(value);
-  };
-
-  return { content, setContent, translateContent, handleChange, language };
+  return { content };
 };
